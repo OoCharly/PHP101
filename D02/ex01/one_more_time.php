b8b9858d4b59ce9f976b2a458094ec9d56ba4aa2
OoCharly <charly.desvernay@gmail.com>
2017-01-10 23:28:12 +0100

    .

--- D02/ex01/one_more_time.php
+++ D02/ex01/one_more_time.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php
 <?PHP
+date_default_timezone_set("Etc/GMT-1");
 if ($argc == 1)
 	exit;
 function	ft_error()
@@ -9,11 +10,24 @@ function	ft_error()
 }
 
 function	check_format($tabt)
-$tabt = preg_split("/[ :]/", $argv[1]);
-if (count($tabt) != 7)
 {
-	echo "Wrong Format\n";
-	exit;
+	$pattern ="/^([Ll]undi|[Mm]ardi|[Mm]ercredi|[Jj]eudi|[Vv]endredi|[Ss]amedi|[Dd]imanche)$/";
+	if (!preg_match($pattern, $tabt[0]))
+		ft_error();
+	$pattern = array("/^[Jj]anvier$/", "/^[Ff]évrier$/", "/^[Mm]ars$/", "/^[Aa]vril$/", "/^[Mm]ai$/", "/^[Jj]uin$/", "/^[Jj]uillet$/", "/^[Aa]o[ûu]t$/", "/^[Ss]eptembre$/", "/^[Oo]ctobre$/", "/^[Nn]ovembre$/", "/^[Dd]écembre$/");
+	$rep = array("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec");
+	$tabt[2] = preg_replace($pattern, $rep, $tabt[2], -1, $err);
+	if (!$err)
+		ft_error();
+	if (!preg_match("/^\d\d?$/", $tabt[1]) || !preg_match("/^\d{4}$/", $tabt[3]) || !preg_match("/^\d\d:\d\d:\d\d$/", $tabt[4]))
+		ft_error();
+	return ($tabt);
 }
-
+$tabt = preg_split("/ /", $argv[1]);
+if (count($tabt) != 5)
+	ft_error();
+$tabt = check_format($tabt);
+$str_d = "$tabt[1] $tabt[2] $tabt[3]T$tabt[4]";
+$out = strtotime($str_d);
+echo "$out\n";
 ?>
